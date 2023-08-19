f354f3d4ac292027144955e807a32563e385ab73
lawand barbarosa <lawandata14@gmail.com>
2023-08-19 15:09:44 +0300

    first commit (fourth lesson)

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var names = ['barbarosa', "uruj,sinanthearch"];
+var numbers = [10, 12, 14, 20];
+numbers.push(25);
+//objects
+var barbarosa = {
+    name: "barbarosa",
+    belt: "blue",
+    age: 20,
+};
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+let names = ['barbarosa',"uruj,sinanthearch"];
+
+
+
+let numbers = [10,12,14,20];
+
+numbers.push(25)
+
+
+//objects
+let barbarosa = {
+    name:"barbarosa",
+    belt:"blue",
+    age:20,
+}
