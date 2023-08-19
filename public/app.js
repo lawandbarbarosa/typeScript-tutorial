4c4ab04f5b4365ec234b794e3ccee3a862018e30
lawand barbarosa <lawandata14@gmail.com>
2023-08-19 22:13:37 +0300

    adding class

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
 "use strict";
-// const anchor = document.querySelector("a");
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details} `;
+    }
+}
+const invOne = new Invoice('barbarosa', "work on the marion website", 250);
+const invTow = new Invoice('lawand', "work on the marion website", 150);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTow);
+console.log(invoices);
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toform = document.querySelector("#tofrom");
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,27 @@
-// const anchor = document.querySelector("a");
+// classes
+class Invoice{
+    client:string ;
+    details:string;
+    amount:number;
+    constructor(c:string,d:string,a:number){
+        this.client= c;
+        this.details = d;
+        this.amount  = a;
+    }
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details} `
+    }
+}
+
+const invOne = new Invoice('barbarosa',"work on the marion website",250)
+const invTow= new Invoice('lawand',"work on the marion website",150)
+
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTow);
+
+console.log(invoices)
 
 const form = document.querySelector(".new-item-form")! as HTMLFormElement;
 
